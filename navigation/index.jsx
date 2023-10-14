5486c3ccd41febf8237fbe6525ee21e94e04eb7b
Prathamesh Nile <prathameshnile1@gmail.com>
2023-10-14 12:18:21 +0530

    Dir setup and added react navigation

---
+++ navigation/index.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from '../components/screens/Login';
+// import Login from '../components/screens/Login';
+
+
+const Stack = createStackNavigator();
+
+export default function Navigator(props){
+	const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+	//API call check isLoggedIn
+
+	return(
+	    <NavigationContainer>
+	     {/*isLoggedIn ? */}
+	      <Stack.Navigator>   	
+	        <Stack.Screen name="Login" component={Login} />
+	        {/*<Stack.Screen name="Signup" component={Signup} />*/}
+	      </Stack.Navigator>
+	      {/*:
+	      <Stack.Navigator>   	
+	        <Stack.Screen name="Home" component={Home} />
+	      </Stack.Navigator>*/}
+	    </NavigationContainer>
+  );
+}
